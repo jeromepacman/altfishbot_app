c94e6667ce2c221f5218ac54116ae35e1ce93874
jeromepacman <jeromepacman@gmail.com>
2022-09-25 18:40:23 +0200

    is_premium option

--- altfishbot/admin.py
+++ altfishbot/admin.py
@@ -6,7 +6,7 @@ from . import models
 @admin.register(models.TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
     list_display = (
-        'telegram_id', 'first_name', 'last_name', 'username', 'has_status', 'role', 'post_count', 'language_code', 'updated_at',
+        'telegram_id', 'first_name', 'last_name', 'username', 'has_status', 'role', 'is_premium', 'post_count', 'language_code', 'updated_at',
         'joined')
     ordering = ('-updated_at',)
     filter_by = ['role', 'post_count']
