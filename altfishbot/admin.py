3d2f14541b300496fb8ef9242801a81bf4445f13
jeromepacman <jeromepacman@gmail.com>
2022-11-26 00:01:58 +0100

    refactor

--- altfishbot/admin.py
+++ altfishbot/admin.py
@@ -6,19 +6,19 @@ from . import models
 @admin.register(models.TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
     list_display = (
-        'telegram_id', 'first_name', 'last_name', 'username', 'has_status', 'role', 'is_premium', 'post_count', 'language_code', 'updated_at',
+        'telegram_id', 'first_name', 'last_name', 'username', 'has_status', 'role', 'post_count', 'warnings', 'is_premium', 'language_code', 'updated_at',
         'joined')
     ordering = ('-updated_at',)
     filter_by = ['role', 'post_count']
-    search_fields = ('telegram_id', 'first_name')
+    search_fields = ['telegram_id', 'first_name', 'username']
     list_editable = ['has_status', 'role']
 
 
 @admin.register(models.TelegramChat)
 class TelegramChatAdmin(admin.ModelAdmin):
-    list_display = ('telegram_id', 'type', 'title', 'username')
+    list_display = ['telegram_id', 'username', 'type', 'title', ]
 
 
 @admin.register(models.TelegramState)
 class TelegramStateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'telegram_user', 'telegram_chat')
+    list_display = ['name', 'telegram_user', 'telegram_chat', ]
