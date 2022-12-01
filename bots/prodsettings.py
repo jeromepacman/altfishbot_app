0a61d4ef606768a14c332b2061005b9875ea2071
jeromepacman <jeromepacman@gmail.com>
2022-12-02 00:36:36 +0100

    requirements.txt

--- bots/prodsettings.py
+++ bots/prodsettings.py
@@ -2,6 +2,7 @@ from . import settings
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+
 
 #SECURE_SSL_REDIRECT = True
