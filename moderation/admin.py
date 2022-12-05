6fc081b2b48b208e01c5ae047006d4bbeb3c290a
jeromepacman <jeromepacman@gmail.com>
2022-12-05 20:09:47 +0100

    admin dashboard ui

--- moderation/admin.py
+++ moderation/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import BannedWord
+from .models import BannedWord, Referral
 
 
 @admin.register(BannedWord)
 class BannedWordAdmin(admin.ModelAdmin):
     list_display = ['banned_word']
+
+
+@admin.register(Referral)
+class ReferralAdmin(admin.ModelAdmin):
+    list_display = ['ref_list']
