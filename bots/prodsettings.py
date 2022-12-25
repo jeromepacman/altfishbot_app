e4a81c05dc7dab61401aeab26800a76667d6451d
jeromepacman <jeromepacman@gmail.com>
2022-12-25 15:08:04 +0100

    settings

--- bots/prodsettings.py
+++ bots/prodsettings.py
@@ -1,3 +1,15 @@
+from settings import *
 
+DEBUG = False
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+#CSRF_TRUSTED_ORIGIN = []
 
 #SECURE_SSL_REDIRECT = True
--- bots/settings.py
+++ bots/settings.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
@@ -28,22 +28,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'debug_toolbar',
     'django_tgbot',
-    'requests_panel',
     'altwhales',
     'altfishbot',
     'moderation',
-    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -82,6 +81,7 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -118,28 +118,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 if not DEBUG:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
     SECURE_SSL_REDIRECT = True
-
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.history.HistoryPanel',
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-    'requests_panel.panel.RequestsDebugPanel',
-
-]
