22d26852824dd2e4fb1aa30309f8bbdfcc39c194
jeromepacman <jeromepacman@gmail.com>
2023-03-26 18:24:26 +0200

    psql

--- bots/settings.py
+++ bots/settings.py
@@ -82,6 +82,24 @@ DATABASES = {
 }
 
 
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': '5432',
+            'CONN_MAX_AGE': None,
+            'CONN_HEALTH_CHECKS': True,
+
+        }
+    }
+
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -104,7 +122,7 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
 
